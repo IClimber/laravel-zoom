768dbfdde1a067f8fac2b66c8e078c309a2d3e2b
IClimber <0969966@gmail.com>
2020-04-02 03:14:09 +0300

    Add refresh token method

--- src/Support/Model.php
+++ src/Support/Model.php
@@ -272,7 +272,7 @@ abstract class Model
     {
         if ($this->hasID()) {
             if (in_array('put', $this->methods) || in_array('patch', $this->methods)) {
-                $this->response = $this->response = $this->client->patch("{$this->getEndpoint()}/{$this->getID()}", $this->updateAttributes());
+                $this->response = $this->client->patch("{$this->getEndpoint()}/{$this->getID()}", $this->updateAttributes());
                 if ($this->response->getStatusCode() == 204) {
                     return $this;
                 } else {
--- src/Zoom.php
+++ src/Zoom.php
@@ -4,8 +4,11 @@ namespace IClimber\Zoom;
 
 use Exception;
 use GuzzleHttp\Client;
+use IClimber\Zoom\Exceptions\InvalidAccessTokenException;
+use IClimber\Zoom\Exceptions\RequestException;
 use IClimber\Zoom\Interfaces\PrivateApplication;
 use IClimber\Zoom\Interfaces\PublicApplication;
+use IClimber\Zoom\Support\Response;
 use Illuminate\Support\Str;
 
 /**
@@ -27,9 +30,11 @@ use Illuminate\Support\Str;
  */
 class Zoom
 {
+    const TOKEN_ENDPOINT = 'https://zoom.us/oauth/token';
+
     protected $client;
 
-    public static function getUserAccessData(string $code, string $redirect_uri, string $grant_type = 'authorization_code')
+    public static function getUserAccessData(string $code, string $redirect_uri)
     {
         $options = [
             'headers' => [
@@ -40,16 +45,54 @@ class Zoom
         ];
 
         $params = [
-            'grant_type' => $grant_type,
+            'grant_type' => 'authorization_code',
             'code' => $code,
             'redirect_uri' => $redirect_uri
         ];
 
-        $response = (new Client($options))->post('https://zoom.us/oauth/token', [
-            'form_params' => $params
-        ]);
+        try {
+            $response = new Response((new Client($options))->post(self::TOKEN_ENDPOINT, [
+                'form_params' => $params
+            ]));
+        } catch (Exception $exception) {
+            $response = new Response($exception->getResponse());
+        }
 
-        return $response->getBody()->getContents();
+        if ($response->getStatusCode() != 200) {
+            throw new RequestException($response->getStatusCode() . ' status code');
+        }
+
+        return $response->getBody();
+    }
+
+    public static function refreshToken(string $refresh_token)
+    {
+        $options = [
+            'headers' => [
+                'Authorization' => 'Basic ' . base64_encode(config('zoom.client_id') . ':' . config('zoom.client_secret')),
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json',
+            ],
+        ];
+
+        $params = [
+            'grant_type' => 'refresh_token',
+            'refresh_token' => $refresh_token
+        ];
+
+        try {
+            $response = new Response((new Client($options))->post(self::TOKEN_ENDPOINT, [
+                'form_params' => $params
+            ]));
+        } catch (Exception $exception) {
+            $response = new Response($exception->getResponse());
+        }
+
+        if ($response->getStatusCode() == 401) {
+            throw new InvalidAccessTokenException($response->getBody()['reason'] ?? 'Invalid Token!');
+        }
+
+        return $response->getBody();
     }
 
     public function __construct(string $userToken = null)
